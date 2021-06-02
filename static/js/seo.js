4be08475e6caccf9a95c4c7ac694f5dd6dd781d4
zhaifanhua <zhaifanhua@foxmail.com>
2021-06-02 22:05:48 +0800

    博客更新

--- static/js/seo.js
+++ static/js/seo.js
@@ -1,7 +1,28 @@
 /* -----------------------360搜索----------------------- */
 (function () {
-  var src = "https://s.ssl.qhres2.com/ssl/ab77b6ea7f3fbf79.js";
-  document.write('<script src="' + src + '" id="sozz"></script>');
+  (function (e) {
+    function t(e) {
+      var t = location.href,
+        n = t.split("").reverse(),
+        r = e.split(""),
+        i = [];
+      for (var s = 0, o = 16; s < o; s++) i.push(r[s] + (n[s] || ""));
+      return i.join("");
+    }
+    var n = /([http|https]:\/\/[a-zA-Z0-9\_\.]+\.so\.com)/gi,
+      r = e.location.href;
+    if (r && !n.test(r) && window.navigator.appName) {
+      var i = "//s.360.cn/so/zz.gif",
+        s = document.getElementById("sozz"),
+        o = s.src.split("?")[1],
+        u = t(o),
+        a = new Image();
+      r && (i += "?url=" + encodeURIComponent(r)),
+        o && (i += "&sid=" + o),
+        u && (i += "&token=" + u),
+        o && (a.src = i);
+    }
+  })(window);
 })();
 
 /* -----------------------百度搜索----------------------- */
